caa992a6d3b9e32fc6de4d76e86991dca43c23e0
Noela-Murugi <murugin254@gmail.com>
2022-08-26 20:54:47 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindromeWord = word.toLowerCase();
+  let reversePalindrome = palindromeWord.split('').reverse().join('');
+  if(  palindromeWord === reversePalindrome){
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
-/* 
+/*
   Add your pseudocode here
+
+  Initialize a variable called palindromeWord that sets word to lowercase
+  Initialize another variable called reversePalindrome that reverses what is stored on palindromeWord variable
+  Have a conditional statement
+    Check if both variable word stings are similar or <noscript>
+      If they are, then return true
+      If not, then return false
+      
+  </noscript>
 */
 
 /*
